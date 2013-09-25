74a72e576cbc1ea5d682db537a1daec0036cfb07
Ricardo Osorio <contacto@xora.org>
2013-09-25 16:01:42 -0500

    Update wpve.php

--- wpve.php
+++ wpve.php
@@ -65,7 +65,7 @@ La Base de datos de Plugins vulnerables es una recolección de proyectos entre l
 
 <body>
 <?php
-//Obteiene la URL actual
+//Obtiene la URL actual
 $localhost = $_SERVER['HTTP_HOST']."/";
 
 //Formulario de dominio a analizar
@@ -88,7 +88,7 @@ $dominio = htmlspecialchars($_POST['dominio'], ENT_QUOTES);
 $path = "wp-content";
 
 if (isset($_POST['analizar']) && !empty($dominio)) {
-	echo "<div align='center' style='color: green;'>Se buscaron ".count(file('vl.bd'))." vulnerabilidades en: <a target='_blank' href='".$protocolo.$dominio.$path."'>".$protocolo.$dominio.$path."/</a> <br> Resultados:</div>";
+	echo "<div align='center' style='color: green;'>Se buscaron ".count(file('vulns.bd'))." vulnerabilidades en: <a target='_blank' href='".$protocolo.$dominio.$path."'>".$protocolo.$dominio.$path."/</a> <br> Resultados:</div>";
 	$dato = file("vl.bd") or exit("<section style='color: orange;' align='center'>Hubo un error al cargar la base de Datos, asegurese de tener los permisos correctos!</section>");
 	foreach($dato as $valor){
 	list($vuln, $referencia, $tipo) = explode("|", $valor);
